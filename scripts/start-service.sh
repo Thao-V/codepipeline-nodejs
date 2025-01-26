e1e9dbcc4cd9cab9ad2067426fc83d0a3f3706dd
thao.vu <thaovu@miu.edu>
2025-01-26 14:33:10 -0600

    tested

--- scripts/start-service.sh
+++ scripts/start-service.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+echo "Debugging deployment environment..." > /home/ec2-user/deploy-debug.log
+echo "User: $(whoami)" >> /home/ec2-user/deploy-debug.log
+echo "PATH: $PATH" >> /home/ec2-user/deploy-debug.log
+echo "Starting PM2..." >> /home/ec2-user/deploy-debug.log
 pm2 start /home/ec2-user/app/src/server.js --name "nodejs-app"
