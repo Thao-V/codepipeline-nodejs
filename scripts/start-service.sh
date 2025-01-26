58e81f094b4b3f7c618a716dcb268b581f48ac25
thao.vu <thaovu@miu.edu>
2025-01-26 14:21:57 -0600

    updated scripts

--- scripts/start-service.sh
+++ scripts/start-service.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd /home/ec2-user/app
-pm2 start src/server.js --name "nodejs-app"
+pm2 start /home/ec2-user/app/src/server.js --name "nodejs-app"
--- scripts/stop-service.sh
+++ scripts/stop-service.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd /home/ec2-user/app
 pm2 stop nodejs-app || true
