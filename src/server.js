83df49cd21ca64aa6c0a2819031f418a52193be8
thao.vu <thaovu@miu.edu>
2025-04-15 11:40:57 -0500

    triggered another pipeline

--- src/server.js
+++ src/server.js
@@ -3,9 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello world 123!")
+  res.send("Welcome to CS516!")
 })
 
 const port = 8080;
-
 app.listen(port, () => console.log(`Listening on port ${port}... `));
